7b37aa7eb644aa387864e3f704a58777f0aab887
ktsb3 <keshava.t.s.b@hotmail.com>
2019-11-08 11:25:35 -0600

    minor changes

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+main.py solve the n-body problem using newton
+Copyright (C) 2019 Jorge Antonio Camarena Pliego (camarenapliego@gmail.com)
+Keshava Tonathiu Sanchez Barbosa (keshava.t.s.b@gmail.com)
+Stephany Dzoara Vargas Mier (stephanydvm@comunidad.unam.mx)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
 
 
 import math
